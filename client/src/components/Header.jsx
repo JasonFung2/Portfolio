8d9fc3c4cd5381f2244fcd3785cec4201586c021
Jason Fung <jason-814@hotmail.com>
2025-01-27 22:26:29 +0000

    remove use client

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,4 @@
 import { Search, Bell, Plus, ChevronDown, BookOpen, BookMarked, Layout, Star } from "lucide-react"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Box, PanelsTopLeft } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 const Header = ({ children }) => {
